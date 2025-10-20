4b29db0a2196464284c1ed20be82dcf8691ceee9
Abdulla01-code <60105012@udst.edu.qa>
2025-10-21 00:56:10 +0300

    persistence.js modified
    I removed any function that related to users as we do not need them anymore and i modified on every function in the persistence layer to connect it to mongo also

--- persistence.js
+++ persistence.js
@@ -1,55 +1,42 @@
 const fs = require('fs/promises')
 
+const mongodb = require("mongodb")
+let db = undefined
+let photos = undefined
+let albums = undefined
+let users = undefined
+
 /**
- * Read user data from JSON file
- * @returns {Promise<Array>} Array of user objects
+ *  Connect to mongo DB
  */
-async function readUserData() {
-    let raw = await fs.readFile('users.json', 'utf8')
-    return await JSON.parse(raw)
+
+async function connect() {
+    if(!db){
+        let client = new mongodb.MongoClient("mongodb+srv://60105012:essam123@cluster0.rvdq9ou.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        await client.connect()
+        db = client.db("infs3201_fall2025")
+        photos = db.collection("photos")
+        albums = db.collection("albums")
+        console.log('Connected to MongoDB')
+    }
 }
 
 /**
- * 
- * Read photo data from JSON file
- * @returns {Promise<Array>} Array of photo objects
+ * Read photo data from MongoDB
+ * @returns {Promise<Array>} Array of album objects
  */
 async function readPhotoData() {
-    let raw = await fs.readFile('photos.json', 'utf8')
-    return await JSON.parse(raw)
+    await connect()
+    return await photos.find({}).toArray()
 }
 
 /**
- * Read album data from JSON file  
+ * Read album data from MongoDB
  * @returns {Promise<Array>} Array of album objects
  */
 async function readAlbumData() {
-    let raw = await fs.readFile('albums.json', 'utf8')
-    return await JSON.parse(raw)
-}
-
-/**
- * Write photo data to JSON file
- * @param {Array} photoList Array of photo objects
- * @returns {Promise<void>}
- */
-async function writePhotoData(photoList) {
-    await fs.writeFile('photos.json', JSON.stringify(photoList, null, 2))
-}
-
-/**
- * Find user by username
- * @param {string} username Username to find
- * @returns {Promise<Object|undefined>} User object or undefined if not found
- */
-async function findUser(username) {
-    let users = await readUserData()
-    for (let user of users) {
-        if (user.username === username) {
-            return user
-        }
-    }
-    return undefined
+    await connect()
+    return await albums.find({}).toArray()
 }
 
 /**
@@ -58,14 +45,10 @@ async function findUser(username) {
  * @returns {Promise<Object|undefined>} Photo object or undefined if not found
  */
 async function findPhoto(photoId) {
-    let photos = await readPhotoData()
-    for (let photo of photos) {
-        if (photo.id === photoId) {
-            return photo
-        }
-    }
-    return undefined
+    await connect()
+    return await photos.findOne({id: photoId})
 }
+
 
 /**
  * Find photos by album ID
@@ -73,15 +56,10 @@ async function findPhoto(photoId) {
  * @returns {Promise<Array>} Array of photos in the album
  */
 async function findPhotosByAlbum(albumId) {
-    let photos = await readPhotoData()
-    let result = []
-    for (let photo of photos) {
-        if (photo.albums.includes(albumId)) {
-            result.push(photo)
-        }
-    }
-    return result
+    await connect()
+    return await albums.find({ albums: { $in: [albumId] } }).toArray()
 }
+
 
 /**
  * Find album by name
@@ -89,14 +67,10 @@ async function findPhotosByAlbum(albumId) {
  * @returns {Promise<Object|undefined>} Album object or undefined if not found
  */
 async function findAlbumByName(albumName) {
-    let albums = await readAlbumData()
-    for (let album of albums) {
-        if (album.name.toLowerCase() === albumName.toLowerCase()) {
-            return album
-        }
-    }
-    return undefined
+    await connect()
+    return await albums.findOne({ name: albumName})
 }
+
 
 /**
  * Update photo in storage
@@ -104,57 +78,27 @@ async function findAlbumByName(albumName) {
  * @returns {Promise<boolean>} True if successful, false if photo not found
  */
 async function updatePhoto(updatedPhoto) {
-    let photos = await readPhotoData()
-    let changed = false
-    
-    for (let i = 0; i < photos.length; i++) {
-        if (photos[i].id === updatedPhoto.id) {
-            photos[i] = updatedPhoto
-            changed = true
-            break
-        }
-    }
-    
-    if (changed) {
-        await writePhotoData(photos)
-    }
-    return changed
-}
-
-
-const mongodb = require("mongodb")
-let db = undefined
-let photos = undefined
-let albums = undefined
-let users = undefined
-
-async function connect() {
-    if(!db){
-        let client = new mongodb.MongoClient("mongodb+srv://60105012:essam123@cluster0.rvdq9ou.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-        await client.connect()
-        db = client.db("infs3201_fall2025")
-        users = db.collection("users")
-        photos = db.collection("photos")
-        albums = db.collection("albums")
-    }
+    await connect()
+    const result = await photos.updateOne(
+        {id: updatedPhoto.id},
+        {$set: updatedPhoto}
+    )
+    return result.modifiedCount === 1
 }
 
 async function getAllUsers() {
-    await connect()
-    let allUsers = await users.find().toArray()
-    console.log(allUsers)
+
+    return []
 }
 
 async function getAllPhotos() {
     await connect()
-    let allPhotos = await photos.find().toArray()
-    console.log(allPhotos)
+    return await photos.find({}).toArray()
 }
 
 async function getAllAlbums() {
     await connect()
-    let allAlbums = await albums.find().toArray()
-    console.log(allAlbums)
+    return await albums.find({}).toArray()
 }
 
 getAllUsers()
@@ -164,10 +108,10 @@ module.exports = {
     getAllUsers,
     getAllPhotos,
     getAllAlbums,
-    writePhotoData,
-    findUser,
     findPhoto,
     findPhotosByAlbum,
     findAlbumByName,
-    updatePhoto
+    updatePhoto,
+    readAlbumData,
+    readPhotoData
 }
